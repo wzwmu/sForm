7843ae9f454c228f714e87a916a07c76c10a25e7
wzwmu <snow.wan@163.com>
2017-09-28 09:09:15 +0800

    init

---
+++ SSHFrom/BO/PublishBO.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SSHFrom.BO
+{
+    [Serializable]
+    public class PublishBO
+    {
+        private ServerBO server;
+        //private bool isCompile;
+        //private string compileSor;
+        //private string compileTarget;
+
+        private string source;
+        private string white;
+        private bool isInc;
+        private DateTime datetimeInc;
+        private string remotePath;
+        private string reg;
+        private string startBefor;
+        private string startup;
+
+        public PublishBO(ServerBO server,string source, string white, bool isInc, DateTime datetimeInc, string remotePath, string reg, string startBefor ,string startup)
+        {
+            this.server = server; 
+            this.source = source;
+            this.white = white;
+            this.isInc = isInc;
+            this.datetimeInc = datetimeInc;
+            this.remotePath = remotePath;
+            this.reg = reg;
+            this.startBefor = startBefor;
+            this.startup = startup;
+        }
+        public ServerBO Server { get => server; set => server = value; }
+        //public bool IsCompile { get => isCompile; set => isCompile = value; }
+        //public string CompileSor { get => compileSor; set => compileSor = value; }
+        //public string CompileTarget { get => compileTarget; set => compileTarget = value; }
+        public string Source { get => source; set => source = value; }
+        public string White { get => white; set => white = value; }
+        public bool IsInc { get => isInc; set => isInc = value; }
+        public DateTime DatetimeInc { get => datetimeInc; set => datetimeInc = value; }
+        public string RemotePath { get => remotePath; set => remotePath = value; }
+        public string Reg { get => reg; set => reg = value; }
+        public string StartBefor { get => startBefor; set => startBefor = value; }
+        public string Startup { get => startup; set => startup = value; }
+    }
+}
---
+++ SSHFrom/ShellHepler.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO; 
+using System.Text;
+using System.Text.RegularExpressions;
+using Tamir.SharpSsh.jsch;
+
+namespace SSHFrom
+{
+    class ShellHepler
+    {
+        MemoryStream outputstream = new MemoryStream();
+        Tamir.SharpSsh.SshStream inputstream = null; 
+        /// <summary> 
+        /// 命令等待标识 
+        /// </summary> 
+
+        List<string> waitMark = new List<string>() { "]#", "]$" };
+        public bool OpenShell(string host, string username, string pwd)
+        {
+            try
+            {
+                inputstream = new Tamir.SharpSsh.SshStream(host, username, pwd);
+                ///我手动加进去的方法。。为了读取输出信息 
+                inputstream.set_OutputStream(outputstream);
+                System.Threading.ThreadPool.QueueUserWorkItem(thread);
+                return inputstream != null;
+            }
+            catch { throw; }
+        }
+        public bool ShellImmediately(string cmd)
+        {
+            if (inputstream == null) return false;
+            inputstream.Write(cmd);
+            inputstream.Flush();
+            System.Threading.Thread.Sleep(200);
+            return true;
+        }
+        public bool Shell(string cmd)
+        {
+            if (inputstream == null) return false;
+            string initinfo = GetAllString();
+            inputstream.Write(cmd);
+            inputstream.Flush();
+            string currentinfo = GetAllString();
+            while (currentinfo == initinfo)
+            {
+                System.Threading.Thread.Sleep(100);
+                currentinfo = GetAllString();
+            }
+            return true;
+        }
+        public event Action<String> OutputEvent;
+        long pos=0;
+        private void thread(Object obj)
+        {
+            while(inputstream != null)
+            {
+                try
+                {
+                    if (pos < outputstream.Position)
+                    {
+                        long len = outputstream.Length;
+                        byte[] buffer = new byte[len - pos];
+                        outputstream.Seek(pos, SeekOrigin.Begin);
+                        outputstream.Read(buffer, 0, buffer.Length);
+                        string outinfo = Encoding.UTF8.GetString(buffer);
+                        outputstream.Flush();
+                        Regex r = new Regex("\\u001b\\[[^m]*m");
+                        if (OutputEvent != null) OutputEvent(r.Replace(outinfo, ""));
+                        pos = outputstream.Position;
+                    }
+                }
+                catch { }
+                System.Threading.Thread.Sleep(200);
+            }
+        }
+        public string GetAllString() 
+        { 
+            string outinfo = Encoding.UTF8.GetString(outputstream.ToArray()); 
+            //等待命令结束字符  
+            while (!outinfo.Trim().EndsWith(waitMark[0]) && !outinfo.Trim().EndsWith(waitMark[1])) 
+            { 
+                System.Threading.Thread.Sleep(200); 
+                outinfo = Encoding.UTF8.GetString(outputstream.ToArray()); 
+            } 
+            outputstream.Flush(); 
+            return outinfo.ToString();
+        }
+        public string GetLastString()
+        {
+            string outinfo = Encoding.UTF8.GetString(outputstream.ToArray());
+            outputstream.Flush();
+            string[] strs = outinfo.Trim().Split(waitMark.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+            return strs[strs.Length - 1];
+        }
+        public void Close()
+        {
+            if (inputstream != null) inputstream.Close();
+            inputstream = null;
+        }
+    }
+}
